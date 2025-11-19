1f4db9a32a6978d1a93fadd30f1de3b509be8040
Morris sardo <zlac176@live.rhul.ac.uk>
2025-11-19 19:07:41 +0000

    delete old file and I have added the new ones.

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Avanvance_distribute_system
+# Files:
+- paxos.ex: main Paxos layer source
+- beb.ex: best effort broadcast (BEB) layer the Paxos layer is built upon. 
+Mostly there to illustrate a possible way to implement layers in Elixir. Can be inlined into paxos.ex
+- test_harness.ex: testing framework
+- paxos_test.ex: collection of unit tests for the Paxos protocol
+- test_script_local.exs: the test driver script for the local tests
+- test_script.exs: the test driver script for the distributed tests
+- uuid.ex: library for generating unique identifiers; used by 
+the test script to generate unique node names
+- seat_reserve.ex: a fault-tolerant implementation of the seat reservation service (requires a working implementation of the Paxos service!)
+- README: this file
+
+# Paxos interface:
+- start(name, participants, upper): start an instance of a Paxos layer process; returns PID
+- propose(pid, value): propose a value via the Paxos process associated with PID. N.B. for testability, this should do nothing except store the input value until start_ballot is invoked (see below).
+- start_ballot(pid): ask the Paxos process process associated with PID to start a new ballot as the leader of that ballot. At this point the process should begin to execute the 2-phase Paxos protocol as the leader.
+
+# Testing
+To test Paxos, customise test_script.exs and/or test_script_local.exs as instructed in the comments. You may wish to comment out the more advanced tests initially.
+
+Make sure there are no any leftover Erlang VMs from previous runs:
+
+killall -9 beam.smp
+
+Launch the test script by entering
+
+iex test_script_local.exs
+
+Or for distributed testing
+ 
+iex test_script.exs
+
+For convenience, helper scripts are also provided (Linux only) 
+
+$ ./run_test_local.sh
+
+or 
+
+$ ./run_test.sh
+
+# Debugging tips - warnings
+1. Make sure you understand any Elixir warnings you receive, e.g. for unused variable names, and that they are indeed harmless.
+2. The test framework may output its own warnings, *even for passing tests*. These warnings come in two flavours: (i) it took an unexpectedly large number of attempts/retries for your solution to come to consensus for the test (ii) there appear to be an unexpectedly large number of messages in some of your process message queues. If you find that your solution passes the simpler tests but fails more complex tests, double-check that you are not receiving any warnings for the passing tests since it may indicate an underlying issue or inefficiency in your solution.  
+
+# Example application
+The file seat_reserve.ex gives a fault-tolerant implementation of the seat
+reservation service from Lab 5 that uses the Paxos service. Its API is slightly
+amended in that the `start` function also requires a list of names
+of the other participating service replicas.
+
+Once your Paxos implementation is passing all the tests, you may wish to try it
+out by customising example_script_local.exs. For convenience, a helper script
+run_example_local.sh is also provided (Linux only). 
+
--- paxos.ex
+++ paxos.ex
@@ -1,145 +1,132 @@
-#############################################################################
-# Paxos implementation – up to Stage 6.5 with UNIQUE BALLOTS
-#
-# API:
-#   Paxos.start(name, participants, upper) :: pid
-#   Paxos.propose(pid, value)              :: :ok
-#   Paxos.start_ballot(pid)                :: :ok
-#
-# Each Paxos process acts as:
-#   - a LEADER (when start_ballot/1 is called)
-#   - an ACCEPTOR (when it receives PREPARE / PROPOSE)
-# and it notifies its upper process with:
-#   {:decide, value}
-#############################################################################
 
 defmodule Paxos do
 
-  # PUBLIC API
-  # name: unique atom for this Paxos replica (e.g., :p1)
-  # participants: list of *names* (atoms) of all Paxos replicas
-  # upper: pid of the upper-layer process to notify when decided
+ 
   def start(name, participants, upper) do
-
-    # this used to to give a ballot number unique to each process.
-    # i have added this to pass test.
-    # it strange because in the real scenarion i multiple process can have same ballot number.  
-    ######
-    #I DONT UNDERSRADN THE POINNT
+    
+    # this is used to give a starting unique value to each participant.
     my_index =
       case Enum.find_index(participants, fn p -> p == name end) do
         nil -> 0
         idx -> idx
       end
 
-    #number of particitpant
-    # WHAT IS THE MEANING OF BALLOT STEP.
-    # WHAT IS BALLOT STEP.
+    
     ballot_step = length(participants)
 
-    # Initial state of this Paxos process
+    
     state = %{
-      name: name,
-      participants: participants,  # all process names (atoms)
-      upper: upper,                # who to notify on decision TO UPPER LAYER.
+      name: name,                    #idenfifier of the process
+      participants: participants,    #list of all parteciapnt
+      upper: upper,                  #pid of upper layer process
 
-      # ballot numbering
-      my_index: my_index,          # 0, 1, 2, ...
-      ballot_step: ballot_step,    # usually = length(participants)
-      next_ballot: my_index + 1,   # 1..n initially, then jumps by ballot_step
+      
+      my_index: my_index,           # my index in the participants list
+      ballot_step: ballot_step,     # step to get unique ballot numbers
+      next_ballot: my_index + 1,    # next ballot number to use
 
     
-      my_value: nil,               # this is the value of the leader.
+      my_value: nil,                # value this process wants to propose
 
-      # acceptor-side state
-      promised_ballot: nil,        # highest ballot I promised
-      accepted_ballot: nil,        # last accepted ballot
-      accepted_value: nil,         # last accepted value
+      
+      promised_ballot: nil,         # highest ballot promised
+      accepted_ballot: nil,         # highest ballot accepted
+      accepted_value: nil,          # value accepted
 
-      # leader-side collections
-      promises: %{},               # from -> {ab, av}
-      accepts: %{},                # from -> true
-
-      decided: false               # has this process decided?
+      
+      promises: %{},                # map of promises received
+      accepts: %{},                 # map of accepts received
+  
+      decided: false                # whether decision has been made   
     }
 
-    # Start the Paxos process loop
+  
     pid = spawn(Paxos, :loop, [state])
     :global.register_name(name, pid)
     pid
   end
 
-  # pid: Paxos process pid
-  # val: value we want to propose
+  # API for upper layer to propose a value.
+  # this is used by the leader to set its value to propose. 
+  # in my case is used bey the test.
   def propose(pid, val) do
     send(pid, {:propose, val})
   end
 
-  # pid: Paxos process pid
-  # Tell this process to start a new ballot as leader.
+  # API for upper layer to start a new ballot.
+  # this is used by the leader to start the ballot process.
+  # in my case is used by the test.
   def start_ballot(pid) do
     send(pid, :start_ballot)
   end
 
 
-  # MAIN LOOP – keeps the Paxos process alive
+  # the main loop of the paxos process.
+  # this loop generally recieve message from all parteciapnt and act accordinly changing the state.
   def loop(state) do
     receive do
 
-      ######################################################################
-      # User sets the value this process wants to propose (leader side)
-      ######################################################################
+      
+      # this is used by the leader(test) to set its value to propose.
       {:propose, val} ->
         loop(%{state | my_value: val})
 
 
-      ######################################################################
-      # Start Phase 1 (with UNIQUE BALLOTS)
-      # Leader sends PREPARE(b) to all participants
-      ######################################################################
+      
+      # this is where the leader starts the ballot process
       :start_ballot ->
+      
         # b is the ballot we are starting now
         b = state.next_ballot
         # IO.puts("Leader #{state.name}: sending PREPARE #{b} to #{inspect state.participants}")
 
-        # Send PREPARE to all participants
+        # leader send prepare to all accpetors.
+        # there is alos condition to chack of process failure. if so I do not do nothing.
+        # self() is the pid of the leader process.
         Enum.each(state.participants, fn name ->
           case :global.whereis_name(name) do
+            # if process is not found we do nothing
+            # this is condition accounting for process failure.
             :undefined -> :ok
-            pid -> send(pid, {:prepare, b, self()})
+            pid -> send(pid, {:prepare, b, self()}) 
           end
         end)
 
-        # Next ballot for THIS process jumps by ballot_step
+        
+        #this update ballot in the state 
         new_state = %{
           state |
-          promises: %{},    # empty map – we will fill this when PROMISES arrive
-          accepts: %{},     # empty map – will be used in Phase 2
+          promises: %{},    
+          accepts: %{},     
           decided: false,
+          #guaranteed unique ballot numbers per leader.
+          #this is smsart trick to avoid ballot collosion. 
           next_ballot: b + state.ballot_step
         }
 
+        # loop with the new state
         loop(new_state)
 
-
-      ######################################################################
-      # ACCEPTOR: handle PREPARE(b, leader_pid)
-      ######################################################################
+      #start PHASE 1:
+      # as i describe about prepare and promise phase.
+      # this message,  send a ballot from leader to all acceptor.
+      # the acceptor will check if the ballot just received is higher than the one it has already promised.
+      # if so they will accept. no otherwise.
       {:prepare, b, leader_pid} ->
-        promised = state.promised_ballot
+        promised = state.promised_ballot # ballor used by accpetor
 
-        # Rule:
-        # - If I already promised a higher ballot, reject
-        # - Else promise this ballot and reply with PROMISE
+    
+        # this condiction check it ballot received it higher of the one already promised.
+        # if so it reject the prepare.
+        # otherwise it accept the prepare and send back a promise to the leader. 
         if promised != nil and b < promised do
           # IO.puts("Acceptor #{state.name}: REJECTING PREPARE #{b}, already promised #{promised}")
           loop(state)
         else
           # IO.puts("Acceptor #{state.name}: ACCEPTING PREPARE #{b} (old promised #{inspect promised})")
+          new_state = %{state | promised_ballot: b} # update the promised ballot in the state.
 
-          new_state = %{state | promised_ballot: b}
-
-          # Reply with PROMISE including my accepted info
+          # acceptor send back to the leader, its promises, the ballot accepted and aknowledge aloso about previously accepted ballot, and value. 
           send(leader_pid, {
             :promise,
             state.name,
@@ -147,35 +134,58 @@ defmodule Paxos do
             state.accepted_ballot,
             state.accepted_value
           })
-
-          loop(new_state)
+          
+          #  recurse with the new state. new_state = %{state | promised_ballot: b} descrive above.
+          loop(new_state) 
         end
 
+      # PHASE 1:
+      # this is stage where the leader recieve the promisethe pormise from accceptor.
+      #i nthis stage the leader first will verfy thr ballot recieved is the same it send it. to avoid old ballot from other leader.
+      # if the ballot is right it will store the promise in a map with all details about the poormose. this inclused the accepted ballot and value from acceptor. the highest value(if there is any) be use by the leader to to decide which value to propose in phase 2. if no value is present the leader will use its own value.
+      # Lastly the leader record all partecipant to to chack if there is quorum(majority) to preceed phase 2.
+      # if quorum not found the process will start the process form prepare again.   
 
-      ######################################################################
-      # Stage 6.3 – LEADER: handle PROMISE(from, b, ab, av)
-      ######################################################################
+      # i wll explain the name of that may be confusing.
+      # from is name of accpetor process.
+      # b ballot
+      # ab acccepted ballot old.
+      # accepted value old.
       {:promise, from, b, ab, av} ->
-        # last started ballot for this leader
+
+        # this is an operation require.
+        # when leader send prepare b = next_ballot and then upgrade next_ballot = next_ballot + ballot_step.
+        # therefore to compare the ballot recieved from acceptor(which i considering current one) i have ot substract at at the next one the ballot _step. I HOPE THIS IS CLEAR.
         current_ballot = state.next_ballot - state.ballot_step
 
         # Ignore PROMISE from an old ballot
         if b != current_ballot do
           # IO.puts("Leader #{state.name}: IGNORING PROMISE from #{from} for old ballot #{b}")
-          loop(state)
+          loop(state) # recurse with the same state.
         else
-          # Store the PROMISE from acceptor
+          # if the ballot is right we store the promise in a map.
           new_promises = Map.put(state.promises, from, {ab, av})
           # IO.puts("Leader #{state.name}: stored PROMISE from #{from} -> {ab=#{inspect ab}, av=#{inspect av}}")
 
+          # we are in stage where we are checking for quorum.
+          # therefore this calculate the majority. NUMBER PARTECIAPANTS/2 + 1.
           majority = div(length(state.participants), 2) + 1
 
+          
+          # this statemtent clarly axplain itself.
+          # it check it the mumber of partecipant that promised is higher of equal half + 1.
+          # if so check if there are any previous values accpeted by acceptor. if so use the highest one.
+          # otherwise use the leader value.
+          # then we send the propose ot all active acceptor.
+          # we update the state and we pass in phase 2.
+          #if there is no quorum we restat from prepare.
           if map_size(new_promises) >= majority do
-            #################################################
-            # MAJORITY PROMISES REACHED – choose value
-            #################################################
-
-            # Find the highest accepted_ballot among all promises
+           
+           # I am pretty sure there is a more elegant way to do this. But it works... 
+           # this satify the condition require by the algorithm Paxos. which is to use vlaue of the highest old ballot accepted by acceptor if there is any.
+           # therefore the Enum first filter into the map only the accepted ballot not nil.
+           # then it use max to get the the highest one.
+           # finally the value_to_propose chekc if there is value releated with higest old ballot, otheriwise use the one of the leader.   
             best =
               new_promises
               |> Enum.filter(fn {_p, {ab2, _v2}} -> ab2 != nil end)
@@ -192,7 +202,10 @@ defmodule Paxos do
 
             # IO.puts("Leader #{state.name}: sending PROPOSE #{inspect value_to_propose} for ballot #{b}")
 
-            # Send PROPOSE to all participants (Phase 2)
+            
+            # this send to all to active acceptor the propose with the value decided above and the ballot.
+            # as above if any cprocess is down we do nothing.
+            # self() is the pid of the leader process.
             Enum.each(state.participants, fn name ->
               case :global.whereis_name(name) do
                 :undefined -> :ok
@@ -200,7 +213,8 @@ defmodule Paxos do
               end
             end)
 
-            # Reset accepts for Phase 2, keep promises and remember proposal
+            # this is the usual action.
+            # this update map state in consequne of the action just done. ACCEPTED VALUE AND PROMISE.
             new_state = %{
               state |
               promises: new_promises,
@@ -208,60 +222,83 @@ defmodule Paxos do
               accepted_value: value_to_propose
             }
 
-            loop(new_state)
+            loop(new_state) # recursive as usual with the new state.
           else
-            # Not enough PROMISES yet – just update state
+            
+            # this is the condition if the quorum is not found.
+            # we update the state with new promises even if quorum is not found. becase we may need them later. and we arewith same ballot.
+            # IF I RESET NAD START FROM PREPARE THE TEST ARE GOING TO FAILS.
             new_state = %{state | promises: new_promises}
             loop(new_state)
           end
         end
 
-
-      ######################################################################
-      # ACCEPTOR: handle PROPOSE(v, b, leader_pid)
-      ######################################################################
+       #PHASE 2:
+       # v is value to propose
+       # b is ballot
+       # leader_pid is pid of the leader.
+       # this is the first part of stage two of paxos.
+       # this is the propose meassage sent from the leader to all the acceptor.
+       # the acceptor first check if the promise are are not lower then the ballot recieved. if so that means thet propose received is from old leader and therefore reject it. this is prevent Safeness violation.
+       # if is not the case th acceptor accept the promise and send back to the leader the acknowledge of accepting the propose.
       {:propose, v, b, leader_pid} ->
+        #variable for promised ballot
         promised = state.promised_ballot
 
-        # Rule 1: reject if ballot < promised_ballot
+        # this is a very important coNdition.
+        # this guarantee the safeness of this stage.
+        # none propose is accepted if actual ballot is less value than the promised ballot. this could be suggest that the propose is from old leader.
+        # otherwise the propose is accepted and the state is updated accordingly.
         if promised != nil and b < promised do
           # IO.puts("Acceptor #{state.name}: REJECTING PROPOSE ballot #{b}, promised #{promised}")
           loop(state)
         else
           # IO.puts("Acceptor #{state.name}: ACCEPTING PROPOSE value #{inspect v} for ballot #{b}")
 
+          #the new state is updated accordingly.  
           new_state = %{
             state |
             accepted_ballot: b,
             accepted_value: v
           }
 
-          # Send ACCEPT back to leader
+          # Send akknowledge back to leader.
           send(leader_pid, {:accept, state.name, b})
           loop(new_state)
         end
 
+       # this is the message sent to the leader from the acceptor to aknowledge the accepting of the propose.
+       # from is name of acceptor
+       # b is ballot
 
-      ######################################################################
-      # Stage 6.5 – LEADER: handle ACCEPT(from, b)
-      ######################################################################
+       # this step is pretty similar to the promise step descrive above.
+       # first leader check if the ballot received is the same it sent. for safeness.
+       # if so it is sotre in map state.
+       # after that it checks for quorum.
+       # if this is the case(found quorum) it change accpeting state in decided and send message to the upper layer and the algorithm ends.
+       # if no quorim is found it just recurse with new state.
       {:accept, from, b} ->
-        # last started ballot for this leader
+        # this is the same exaplination as above for promise.
         current_ballot = state.next_ballot - state.ballot_step
 
         # Ignore ACCEPT from an old ballot
         if b != current_ballot do
           # IO.puts("Leader #{state.name}: ignoring ACCEPT from #{from} for old ballot #{b}")
-          loop(state)
+          loop(state) # recurse with the same state.
         else
-          # Store the ACCEPT 
+         
+          #store the accept in map state.
           new_accepts = Map.put(state.accepts, from, :accepted)
           # IO.puts("Leader #{state.name}: stored ACCEPT from #{from}")
 
+          #calculate for majority  
           majority = div(length(state.participants), 2) + 1
 
+          # check for quorum and the decided state is not true(already decided).
           if map_size(new_accepts) >= majority and state.decided == false do
-            # Final decided value:
+
+            # IO.puts("Leader #{state.name}: QUORUM OF ACCEPTS RECEIVED for ballot #{b}, DECIDING VALUE")
+            # this guarantee that if none value was accepted before by acceptor the leader will use its own value.
             decided_value =
               if state.accepted_value != nil do
                 state.accepted_value
@@ -269,10 +306,10 @@ defmodule Paxos do
                 state.my_value
               end
 
-            # Notify upper layer – format {:decide, value}
+            # notify upper layer about the decided value.
             send(state.upper, {:decide, decided_value})
 
-            # Broadcast DECIDE to all participants
+            # send decided value to all participants.
             Enum.each(state.participants, fn name ->
               case :global.whereis_name(name) do
                 :undefined -> :ok
@@ -280,6 +317,7 @@ defmodule Paxos do
               end
             end)
 
+            # update state accordingly.
             new_state = %{
               state |
               accepts: new_accepts,
@@ -288,16 +326,15 @@ defmodule Paxos do
 
             loop(new_state)
           else
-            # Not enough ACCEPTs yet – keep waiting
+
+            # no quorum found we just recurse with new state.
             new_state = %{state | accepts: new_accepts}
             loop(new_state)
           end
         end
 
 
-      ######################################################################
-      # DECIDE handler – all processes receive it
-      ######################################################################
+      # this is the final message sent from leader to the upper layer to notify the decided value. 
       {:decide, value} ->
         # Only act once
         if not state.decided do
@@ -312,10 +349,7 @@ defmodule Paxos do
 
         loop(new_state)
 
-
-      ######################################################################
-      # Stop (not really used in tests)
-      ######################################################################
+        #this is used to stop the process.
       :stop ->
         :ok
     end
